a43615bd6feec865bbf7170db22f116fd1c7e937
Stiven <olajhace2002@gmail.com>
2023-11-29 20:20:46 -0500

    First deploy

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FaExternalLinkAlt } from "react-icons/fa";
+
+const Card = ({image, tittle, description, style, link}) => {
+  return (
+    <div className={`flex justify-around  ${style} `}>
+      <a className='w-[50%] relative group' href={link}> <FaExternalLinkAlt className='hidden absolute group-hover:block z-10 left-32 top-16 text-3xl' /> <img className='  rounded-2xl group-hover:blur-[2px]' src={image} alt="" /></a>
+      <div className='flex flex-col items-center w-[40%] justify-around'>
+          <h3 className='text-2xl font-bold' >{tittle}</h3>
+          <p className='pb-10 text text-gray-400 text-center'>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/layout/Body.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import About from '../components/About'
+import Projects from '../components/Projects'
+import Technologies from '../components/Technologies'
+
+const Body = () => {
+  return (
+    <>
+      <About/>
+      <Projects/>
+      <Technologies/>
+    </>
+  )
+}
+
+export default Body
