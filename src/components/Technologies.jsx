9dfe161f7ad4db3f13264e58c5f332eea553404e
Stiven <olajhace2002@gmail.com>
2023-11-29 23:14:04 -0500

    finals

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -3,14 +3,13 @@ import javaIcon from "../assets/javaIcon.webp"
 import javascIcon from "../assets/javascIcon.webp"
 import html from "../assets/htmlIcon.webp"
 import css from "../assets/cssIcon.webp"
-import react from "../assets/reactIcon.webp"
-import Icon from './Icon'
+import react from "../assets/react.png"
 
 const Technologies = () => {
   return (
-    <div className='h-[80vh] text-6xl text-white text-center justify-evenly'>
+    <div className='h-[80vh] text-6xl text-white text-center justify-evenly py-[5vh]'>
       <h2 >Technologies</h2>
-      <div className='grid w-[85vw] h-[40vh] grid-cols-3 gap-8 items-center mt-[15vh] ml-[3vh]'>
+      <div className='grid w-[65vw] h-[10vh] grid-cols-3 gap-8 items-center mt-[15vh] ml-[8vh]'>
         <img src={javaIcon} alt="" />
         <img src={html} alt="" />
         <img src={javascIcon} alt="" />
